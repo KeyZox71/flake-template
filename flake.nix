b3cdd64f2ec6d0e3ae8cce635965cd659d5dc9c9
Adam JOLY <adjoly@student.42angouleme.fr>
2024-10-31 15:56:45 +0100

    「🔨」 fix(flakeTemplate): Fixed templates path

--- flake.nix
+++ flake.nix
@@ -4,17 +4,17 @@
 	outputs = { self }: {
 		templates = {
 			c-cpp_42 = {
-				path = "./c-cpp_42";
+				path = ./c-cpp_42;
 				description = "My flake template for C of CPP project at 42 school";
 			};
 
 			c-cpp = {
-				path = "./c-cpp";
+				path = ./c-cpp;
 				description = "My flake template for C of CPP project at 42 school";
 			};
 
 			go = {
-				path = "./go";
+				path = ./go;
 				description = "My flake template for GO developement";
 			};
 		};
